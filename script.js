0b19457dee16bba397284b10057acc9b2cc13c67
Phathaphon Sawettaboot <132444206+PhathaphonSawettaboot@users.noreply.github.com>
2023-11-20 00:15:52 +0700

    finished

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 document.getElementById('moreBtn').addEventListener('click', function() {
-    var newFlower = new Image();
-    newFlower.src = 'path_to_your_sunflower_image.png'; // Replace with your sunflower image path
-    newFlower.classList.add('sunflower');
-    // Randomize the position of the new sunflower
-    newFlower.style.left = Math.random() * 100 + '%';
-    newFlower.style.bottom = Math.random() * 50 + '%'; // Adjust if you want different heights
+    for (let i = 0; i < 2; i++) {
+        var newFlower = new Image();
+        newFlower.src = 'asset/sunflower.png'; // Replace with your sunflower image path
+        newFlower.classList.add('sunflower');
+        // Randomize the position of the new sunflower
+        newFlower.style.left = Math.random() * 100 + '%';
+        newFlower.style.bottom = Math.random() * 50 + '%'; // Adjust if you want different heights
+
+        // Set size of the new flower
+        newFlower.style.width = '100px'; // Adjust the width as needed
+        newFlower.style.height = 'auto'; // This keeps the aspect ratio of the image
 
     document.querySelector('.background').appendChild(newFlower);
+    }
+
+    // Display the text
+    var displayText = document.getElementById('displayText');
+  if (displayText.style.display === 'none' || displayText.style.display === '') {
+    displayText.style.display = 'block';
+  } else {
+    displayText.style.display = 'none';
+  }
+});
